e660ff3c5bdc543c99d80bc751d0fbbe7a413390
Guilherme Ramos Mendes <guilhermerm.ciandt@sulamerica.com.br>
2020-03-25 17:48:16 -0300

    Application

---
+++ poc-rabbitmq-consumer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace poc_rabbitmq_consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*******Receiving messages*******");
+            var rabbitMqClient = new RabbitMqClient();
+            rabbitMqClient.Listen();
+        }
+    }
+}
